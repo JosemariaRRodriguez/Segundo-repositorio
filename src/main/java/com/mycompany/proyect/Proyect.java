8c2fa90e3889aa1d34ab6189979ae8f719ad2006
JosemariaRRodriguez <119985478+JosemariaRRodriguez@users.noreply.github.com>
2024-06-26 23:22:58 -0600

    Archivo subido una vez

---
+++ src/main/java/com/mycompany/proyect/Proyect.java
@@ -0,0 +1,14 @@
+package com.mycompany.proyect;
+
+public class Proyect {
+
+    public static void main(String[] args) {
+        /*System.out.println("Hello World!");
+        System.out.println("Soy Josemaria");
+        System.out.println("GTA 6");
+        System.out.println("Soy una linea");
+        System.out.println("Prueba de lineas");*/
+        
+        byte variable1;
+    }
+}
