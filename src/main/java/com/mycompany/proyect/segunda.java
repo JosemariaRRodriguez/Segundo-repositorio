9ae3aff8fecbd3374f14785f13bffa3c4dc5234c
JosemariaRRodriguez <119985478+JosemariaRRodriguez@users.noreply.github.com>
2024-06-27 02:08:32 -0500

    Ejemplo de como subir java a git

---
+++ src/main/java/com/mycompany/proyect/segunda.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.proyect;
+
+
+public class segunda {
+    public static void main(String[] args) {
+        System.out.println("Holaaaaaaaaaaa");
+    }
+}
---
+++ src/main/java/com/mycompany/proyect/tercera.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.proyect;
+
+
+public class tercera {
+    public static void main(String[] args) {
+        System.out.println("Hola tercero");
+    }
+}
