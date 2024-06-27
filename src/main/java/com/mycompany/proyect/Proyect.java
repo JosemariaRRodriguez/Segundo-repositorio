ae7b689a0c42ff7c32e54cbf583afbfb603c8d64
JosemariaRRodriguez <119985478+JosemariaRRodriguez@users.noreply.github.com>
2024-06-27 01:45:24 -0500

    carga inicial

--- src/main/java/com/mycompany/proyect/Proyect.java
+++ src/main/java/com/mycompany/proyect/Proyect.java
@@ -3,12 +3,14 @@ package com.mycompany.proyect;
 public class Proyect {
 
     public static void main(String[] args) {
+        int aux;
         /*System.out.println("Hello World!");
         System.out.println("Soy Josemaria");
         System.out.println("GTA 6");
         System.out.println("Soy una linea");
         System.out.println("Prueba de lineas");*/
         
-        byte variable1;
+        int hol=0;
+        
     }
 }
