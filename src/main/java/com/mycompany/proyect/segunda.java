d0bf41998adea019f364d52f9c975c54b8194613
JosemariaRRodriguez <119985478+JosemariaRRodriguez@users.noreply.github.com>
2024-06-27 02:33:13 -0500

    Ejemplo de como subir java a git segunda

--- src/main/java/com/mycompany/proyect/segunda.java
+++ src/main/java/com/mycompany/proyect/segunda.java
@@ -5,5 +5,6 @@ package com.mycompany.proyect;
 public class segunda {
     public static void main(String[] args) {
         System.out.println("Holaaaaaaaaaaa");
+        System.out.println("Este es un segundo cambio que hice en la clase segunda");
     }
 }
